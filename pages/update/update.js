f65e20226bc001e4f9ae3aadd460faac14128fe7
Don <772528797@qq.com>
2017-10-21 23:54:22 +0800

    完成页面的开发

--- pages/update/update.js
+++ pages/update/update.js
@@ -1,66 +1,48 @@
 // pages/update/update.js
 Page({
 
-  /**
-   * 页面的初始数据
-   */
-  data: {
-  
-  },
+	/**
+	 * 页面的初始数据
+	 */
+	data: {
+		update: null,
+		deptArray: [
+			'A部门', 'B部门', 'C部门', 'D部门', 'E部门'
+		],
+		deptIndex: 0,
+		subjectArray: [
+			'A学科', 'B学科', 'C学科', 'D学科', 'E学科'
+		],
+		subjectIndex: 0
+	},
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-  
-  },
+	/**
+	 * 生命周期函数--监听页面加载
+	 */
+	onLoad: function (options) {
+		let deptIdx = this.data.deptArray.indexOf('C部门');
+		let subjectIdx = this.data.subjectArray.indexOf('B学科');
+		this.setData({
+			update: {
+				name: '张三',
+				phone: '13266668888',
+				gender: '男',
+				deptName: 'C部门',
+				subject: 'B学科'
+			},
+			deptIndex: deptIdx,
+			subjectIndex: subjectIdx
+		});
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
-  }
+	},
+	bindDeptPickerChange: function(e) {
+		this.setData({
+			deptIndex: e.detail.value
+		});
+	},
+	bindSubjectPickerChange: function(e) {
+		this.setData({
+			subjectIndex: e.detail.value
+		});
+	}
 })
--- pages/user/user.js
+++ pages/user/user.js
@@ -27,52 +27,17 @@ Page({
 		})
 	},
 
-	/**
-	 * 生命周期函数--监听页面初次渲染完成
-	 */
-	onReady: function () {
-
+	callPhone: function(e) {
+		wx.makePhoneCall({
+			phoneNumber: e.currentTarget.dataset.phone
+		})
 	},
-
-	/**
-	 * 生命周期函数--监听页面显示
-	 */
-	onShow: function () {
-
-	},
-
-	/**
-	 * 生命周期函数--监听页面隐藏
-	 */
-	onHide: function () {
-
-	},
-
-	/**
-	 * 生命周期函数--监听页面卸载
-	 */
-	onUnload: function () {
-
-	},
-
-	/**
-	 * 页面相关事件处理函数--监听用户下拉动作
-	 */
-	onPullDownRefresh: function () {
-
-	},
-
-	/**
-	 * 页面上拉触底事件的处理函数
-	 */
-	onReachBottom: function () {
-
-	},
-
 	/**
 	 * 用户点击右上角分享
 	 */
 	onShareAppMessage: function () {
-
+		return {
+			title: '张三的名片'
+		}
 	}
 })
